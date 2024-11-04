5a160245e6a47fe1cc32cdbbba005a63e70b2acf
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 15:20:08 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace _3_Longest_Substring_Without_Repeating_Characters {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int max = 0;
+        int start = 0;
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++) {
+            if (dict.ContainsKey(s[i])) {
+                start = Math.Max(start, dict[s[i]] + 1);
+            }
+            dict[s[i]] = i;
+            max = Math.Max(max, i - start + 1);
+        }
+        return max;
+
+    }
+}
